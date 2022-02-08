733e162c61740572b6086a37a95c9bf49e413468
Cedric Gasser <cedigasser@gmail.com>
2022-02-08 19:16:25 +0100

    Center div

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,23 @@
-<h1>Welcome to my portfolio website</h1>
+<div>
+    <h1>Welcome to my portfolio website</h1>
+</div>
+<div>
+    <h3>Yes, there is nothing here yet.</h3>
+</div>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
     * {
         font-family: Nunito, Roboto, system-ui, sans-serif;;
+    }
+    div {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+    h3 {
+        margin-top: 100vh;
     }
 </style>
