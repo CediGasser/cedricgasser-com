5d4b5672c545f84ba379cfd769bf10764e2162b1
Cedric Gasser <cedigasser@gmail.com>
2022-05-14 11:29:38 +0200

    Add scroll snapping

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,19 @@
 
 <Seo title="Cedric Gasser" keywords="Webdev Svelte Portfolio" type="Portfolio" description="My portfolio website. One of the moste minimalistic designed website that exists, although hopefully there will be some more content in the future." image="/social_preview.png"/>
 
-<div>
-    <h1>Welcome to my portfolio website</h1>
-</div>
-<div>
-    <h2>Yes, there is nothing here yet.</h2>
-</div>
+<main>
+    <div class="wrapper">
+        <section class="hero">
+            <h1 class="text-center">Welcome to my portfolio website</h1>
+        </section>
+        <section class="description">
+            <h2 class="text-center">One of the most minimalistic designed websites out there.</h2>
+        </section>
+        <section class="noContent">
+            <h2 class="text-center">Yes, there is nothing here yet.</h2>
+        </section>
+    </div>
+</main>
 
 <style>
     /*design base: https://hihayk.github.io/shaper/#Nunito,%20sans-serif/1.4/1/2.98/0.8/2/0.5/314/100/46/5/0.5/2/false  */
@@ -18,16 +25,28 @@
     * {
         font-family: Nunito, Roboto, system-ui, sans-serif;;
     }
-    div {
-        text-align: center;
+    main {
+        overflow-y: scroll;
+    }
+    .wrapper {
+        overflow-y: scroll;
         height: 100vh;
+        scroll-snap-type: y mandatory;
+    }
+    .wrapper > section:nth-child(even) {
+        background-color: #8d8d8d;
+    }
+    .wrapper > section:nth-child(odd) {
+        background-color: #ff9d00;
+    }
+    section {
         display: flex;
-        align-items: center;
         justify-content: center;
         flex-direction: column;
+        height: 100vh;
+        scroll-snap-align: start;
     }
-    h2 {
-        margin-top: 100vh;
-        margin-bottom: 40vh;
+    .text-center {
+        text-align: center;
     }
 </style>
