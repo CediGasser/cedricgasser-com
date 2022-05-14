d87ce9e529fde3c265618b41d7790671952d648a
Cedric Gasser <cedigasser@gmail.com>
2022-05-14 12:46:21 +0200

    Fix 2 scrollbars

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,19 +4,15 @@
 
 <Seo title="Cedric Gasser" keywords="Webdev Svelte Portfolio" type="Portfolio" description="My portfolio website. One of the moste minimalistic designed website that exists, although hopefully there will be some more content in the future." image="/social_preview.png"/>
 
-<main>
-    <div class="wrapper">
-        <section class="hero">
-            <h1 class="text-center">Welcome to my portfolio website</h1>
-        </section>
-        <section class="description">
-            <h2 class="text-center">One of the most minimalistic designed websites out there.</h2>
-        </section>
-        <section class="noContent">
-            <h2 class="text-center">Yes, there is nothing here yet.</h2>
-        </section>
-    </div>
-</main>
+<section>
+    <h1 class="text-center">Welcome to my portfolio website</h1>
+</section>
+<section>
+    <h2 class="text-center">One of the most minimalistic designed websites out there.</h2>
+</section>
+<section>
+    <h2 class="text-center">Yes, there is nothing here yet.</h2>
+</section>
 
 <style>
     /*design base: https://hihayk.github.io/shaper/#Nunito,%20sans-serif/1.4/1/2.98/0.8/2/0.5/314/100/46/5/0.5/2/false  */
@@ -25,18 +21,17 @@
     * {
         font-family: Nunito, Roboto, system-ui, sans-serif;;
     }
-    main {
-        overflow-y: scroll;
-    }
-    .wrapper {
-        overflow-y: scroll;
+    :global(body) {
         height: 100vh;
+		margin: 0;
+    }
+    :global(body, html) {
         scroll-snap-type: y mandatory;
     }
-    .wrapper > section:nth-child(even) {
+    :global(body) > section:nth-child(even) {
         background-color: #8d8d8d;
     }
-    .wrapper > section:nth-child(odd) {
+    :global(body) > section:nth-child(odd) {
         background-color: #ff9d00;
     }
     section {
