883d1688e0ae5c85de2e62ddbdd499c7531eeb60
Cedric <cedigasser@gmail.com>
2023-05-01 00:14:11 +0200

    Optimizing for vite import

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,7 +1,7 @@
 import type { Post } from '$lib/types.js';
 
 export const load = async ({ params }) => {
-  const post = await import(`/src/posts/${params.slug}/${params.slug}.md`)
+  const post = await import(`../../../posts/${params.slug}/${params.slug}.md`)
   const metadata: Omit<Post, 'slug'> = post.metadata
   const Content = post.default
 
